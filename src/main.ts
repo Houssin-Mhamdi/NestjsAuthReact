20be88cb3ed77036257d9668c23b2bd52c850c3c
houssinCarnelian <houssin.carnelian@gmail.com>
2023-07-17 16:44:58 +0100

    add the GlobalPrefix api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api")
   app.enableCors({
   origin:["http://localhost:3000"],
   credentials:true
