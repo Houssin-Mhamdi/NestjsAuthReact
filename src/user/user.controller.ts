8a0404b3044bd798c66d4bd918132c5f7fbef1c2
houssinCarnelian <houssin.carnelian@gmail.com>
2023-07-17 09:02:53 +0100

    try another solution to hash the password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,23 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import * as bcrypt from 'bcrypt';
 
 @Controller('user')
 export class UserController {
     constructor( private userService:UserService){}
     @Post("register")
-    register(@Body() body:any){
+   async register(@Body() body:any){
         if(body.password !== body.password_confirm){
             throw new BadRequestException("password do not match")
         }
+        const saltOrRounds = 12;
+        const password = body.password;
+        
         return this.userService.save({
             firstName: body.firstName,
             lastName: body.lastName,
             email: body.email,
-            password:
+            password: await bcrypt.hash(password,saltOrRounds)
         })
     }
 }
