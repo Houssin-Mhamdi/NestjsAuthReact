9bddc34afa3197c9380c50d3950fe31476c8f629
houssinCarnelian <houssin.carnelian@gmail.com>
2023-07-17 16:43:45 +0100

    adding creadentials to true to retrive the cookies without the cors essue

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin:["http://localhost:3000"]});
+  app.enableCors({
+  origin:["http://localhost:3000"],
+  credentials:true
+}
+);
   app.use(cookieParser());
   await app.listen(3008);
 }
