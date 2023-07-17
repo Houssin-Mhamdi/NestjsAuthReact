7d8d43d4a788186796946c327a1ce20410e898bc
houssinCarnelian <houssin.carnelian@gmail.com>
2023-07-17 08:54:37 +0100

    hashing the password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    
+    constructor( private userService:UserService){}
     @Post("register")
     register(@Body() body:any){
-        return body
+        if(body.password !== body.password_confirm){
+            throw new BadRequestException("password do not match")
+        }
+        return this.userService.save({
+            firstName: body.firstName,
+            lastName: body.lastName,
+            email: body.email,
+            password:
+        })
     }
 }
